af202c2657adad65daf565ab498fe113713ab0e0
Paul Kinlan <paulkinlan@google.com>
2011-04-01 05:57:12 +0100

    Adding some Paul Irish magic

--- marshall.js
+++ marshall.js
@@ -1,5 +1,37 @@
 var marshall = function() {
-
+  // Some Paul Irish magic
+  if (!window.matchMedia){
+    window.matchMedia = (function(doc, undefined){
+      var bool,
+          docElem = doc.documentElement,
+          fakeBody = doc.createElement('body'),
+          testDiv = doc.createElement('div');
+                                       
+       testDiv.setAttribute('id','ejs-qtest');
+       fakeBody.appendChild(testDiv);
+                                              
+       return function(q){
+         var styleBlock = doc.createElement('style'),
+             cssrule = '@media '+q+' { #ejs-qtest { position: absolute; } }';
+                                                                           
+         styleBlock.type = "text/css"; //must set type for IE! 
+         if (styleBlock.styleSheet){ 
+           styleBlock.styleSheet.cssText = cssrule;
+         } 
+         else {
+           styleBlock.appendChild(doc.createTextNode(cssrule));
+         } 
+         docElem.insertBefore(fakeBody, docElem.firstChild);
+         docElem.insertBefore(styleBlock, docElem.firstChild);
+         bool = ((window.getComputedStyle ? window.getComputedStyle(testDiv,null) : testDiv.currentStyle)['position'] == 'absolute');
+         docElem.removeChild(fakeBody);
+         docElem.removeChild(styleBlock);
+                                                                                               
+         return { matches: bool, media: q };
+       };
+    })(document);
+  }
+  
   function Matcher(query, callback) {
      this.query = query;
      this.callback = callback;
