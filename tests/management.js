1ea02a3ea824f472b5b9bf92979688a3f0b4ff90
Paul Kinlan <paulkinlan@google.com>
2011-04-02 03:13:09 +0100

    Fixing tests and renaming to formfactor.js

--- tests/management.js
+++ tests/management.js
@@ -1,6 +1,5 @@
 describe("management", function() {
   it("should initialize", function() {
-     var m = new marshall();
-     expect(m).not.toBeNull();
+     expect(formfactor).not.toBeNull();
   });
 });
